9584d90f89b5c1d520f5390e475be0172234b964
Max8Well <youkt1531@naver.com>
2023-05-01 14:20:00 +0900

    230501 user profile finish

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -103,7 +103,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                       fit: BoxFit.cover,
                       placeholder: 'assets/images/killbill1.jpeg',
                       image:
-                          'https://source.unsplash.com/random/200x${355 + index}',
+                          'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20100928_50%2Fmahler_1285652989422formd_JPEG%2FKillBillVol11.jpg&type=sc960_832',
                     ),
                   ),
                   Gaps.v10,
